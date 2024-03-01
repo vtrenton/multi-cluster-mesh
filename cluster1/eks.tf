78b8658ea3c2fe0fdb4e246c7757b5c0b058d789
Trenton VanderWert <trenton.vanderwert@gmail.com>
2024-02-29 22:55:18 -0600

    cleaned up eks names
    Signed-off-by: Trenton VanderWert <trenton.vanderwert@gmail.com>

--- cluster1/eks.tf
+++ cluster1/eks.tf
@@ -15,8 +15,8 @@ resource "aws_eks_cluster" "cluster1" {
     role_arn = aws_iam_role.eks_cluster_role.arn
     vpc_config {
         subnet_ids = [
-            aws_subnet.rancher_master_a.id,
-            aws_subnet.rancher_master_b.id
+            aws_subnet.sub_a.id,
+            aws_subnet.sub_b.id
         ]
 
         endpoint_private_access = true
@@ -25,22 +25,22 @@ resource "aws_eks_cluster" "cluster1" {
 
     encryption_config {
         provider {
-            key_arn = aws_kms_key.rancher_secrets.arn
+            key_arn = aws_kms_key.secret_key.arn
         }
         resources = ["secrets"]
     }
     
     depends_on = [
         aws_vpc.cluster_lan,
-        aws_internet_gateway.rancher_cluster_gw,
-        aws_subnet.rancher_master_a,
-        aws_subnet.rancher_master_b
+        aws_internet_gateway.cluster_gw,
+        aws_subnet.sub_a,
+        aws_subnet.sub_b
     ]
 }
 
 # launch config for workers
-resource "aws_launch_template" "rancher_workers" {
-    name_prefix   = "rancher-cow-"
+resource "aws_launch_template" "cluster1_workers" {
+    name_prefix   = "worker-"
     instance_type = var.instance_size
     
     # ssh key name
@@ -54,13 +54,13 @@ resource "aws_launch_template" "rancher_workers" {
 }
 
 # Create a node group for the workers
-resource "aws_eks_node_group" "rancher_node_group" {
-  cluster_name    = aws_eks_cluster.rancher_cluster.name
+resource "aws_eks_node_group" "cluster1_node_group" {
+  cluster_name    = aws_eks_cluster.cluster1.name
   node_group_name = var.node_group_name
   node_role_arn   = aws_iam_role.eks_worker_role.arn
   subnet_ids      = [
-    aws_subnet.rancher_master_a.id,
-    aws_subnet.rancher_master_b.id
+    aws_subnet.sub_a.id,
+    aws_subnet.sub_b.id
   ]
 
   # Rancher scaling recommendations
@@ -72,11 +72,11 @@ resource "aws_eks_node_group" "rancher_node_group" {
 
   # Specify the launch template and version
   launch_template {
-    id      = aws_launch_template.rancher_workers.id
+    id      = aws_launch_template.cluster1_workers.id
     version = "$Latest"
   }
 
   depends_on = [
-    aws_eks_cluster.rancher_cluster
+    aws_eks_cluster.cluster1
   ]
 }
